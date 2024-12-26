3ac9723e5604d0a79fa9e936338ef186725718a3
Lucas Araújo <lucasfelipemk@gmail.com>
2024-12-25 22:49:18 -0300

    :wrench: chore: created site config file

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'stepp.per',
+  url: 'http://localhost:3000/',
+  description:
+    'A multi-step form solution built with Next.js 15, React 19, and TypeScript.',
+  links: {
+    github: 'https://github.com/LFeli',
+    linkedin: 'https://www.linkedin.com/in/lf-araujo/',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
